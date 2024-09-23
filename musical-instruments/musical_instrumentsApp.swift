465c29be003f1303a7ad78a22c1a1c7980e66225
Amund Ring <60844467+Amund-Ring@users.noreply.github.com>
2024-09-23 20:47:52 +0200

    Initial Commit

---
+++ musical-instruments/musical_instrumentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  musical_instrumentsApp.swift
+//  musical-instruments
+//
+//  Created by Amund Ring on 23/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct musical_instrumentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
